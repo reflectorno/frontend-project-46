facbfb9c9a25515649dcfa4ee9e2df7db7210aec
Sergei Kravchenko <reflectorno@gmail.com>
2024-09-24 02:35:29 +0300

    add option and arguments to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,8 @@ const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.9.0')
+.helpOption('-h, --help', 'output usage information')
+.option('-f, --format [type]', 'output format')
+.arguments('<filepath1> <filepath2>')
 
 program.parse();
